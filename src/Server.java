86bbc70bf1582f6c673adca89658c91357d8bd92
Rishav Nath Pati <patirishavnath@gmail.com>
2020-09-13 23:22:19 +0530

    first commit

---
+++ src/Server.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Server extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    Server() {
+        setSize(450, 700);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Server().setVisible(true);
+
+    }
+
+}
